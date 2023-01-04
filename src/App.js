5014e510f8a24752d41be82e0627a806796213f2
Vishalini-2003 <vish20226.it@rmkec.ac.in>
2023-01-05 00:20:53 +0530

    Healthify

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css"
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Homepage} from './Homepage';
+import Login from './Login';
+import Product from './Product';
+
+
+
+
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    
+    
+    <BrowserRouter>
+   
+      <Routes>
+        <Route path="/" element={< Homepage />} />
+        <Route path="Login" element={<Login />} />
+        <Route path="Product" element={<Product />} />
+      </Routes>
+    </BrowserRouter>
+    
+   
   );
 }
 
